a5b555047f28b97308f8a250c67c76b6860fc4d9
sdx0909 <dixitsaurabh092@gmail.com>
2025-04-25 16:20:39 +0530

    done getallcourses-and-getlectur

--- server/routes/course.routes.js
+++ server/routes/course.routes.js
@@ -4,13 +4,14 @@ import {
   getAllCourses,
   getLecturesByCourseId,
 } from "../controllers/course.controller.js";
+import { isLoggedIn } from "../middlewares/auth.middleware.js";
 
 // creating the router
 const router = Router();
 
 // creating the sub-routes of "/api/v1/user"
 router.route("/").get(getAllCourses); // another-approach
-router.get("/:id", getLecturesByCourseId);
+router.get("/:id", isLoggedIn, getLecturesByCourseId);
 
 // exporting the router
 export default router;
